5ed51bf6c1fec48fcc752a5ea758d66f4c493fb6
Alessia Ture <92104146+a-ture@users.noreply.github.com>
2024-01-29 12:21:37 +0100

    add plot graph

--- virus-taxonomy-classification-main/dna/chaos.py
+++ virus-taxonomy-classification-main/dna/chaos.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+from math import sqrt
 from skimage.segmentation import slic
 from complexcgr import FCGR
 from visualize import *
@@ -56,8 +57,9 @@ class ChaosGraph:
                             attr[f'{n}'] = 0
                 # e la posizione in termini spaziali
                     attr["position"] = (i,j)
-                    self.graph_chaos.add_node(value, **attr)
-
+                    attr["value"] = value
+                    #trovare un nome migliore univoco
+                    self.graph_chaos.add_node((i,j), **attr)
         self.node_attr = list(attr.keys())
 
         # Crea archi
@@ -71,8 +73,31 @@ class ChaosGraph:
         # feature per archi la distanza euclidea  tra i nodi che li compongono
                 pass
 
+    def plot_graph(self):
+       pos = nx.circular_layout(self.graph_chaos)
+       plt.figure()
+       nx.draw(
+            self.graph_chaos,
+            pos,
+            edge_color='black',
+            width=1,
+            linewidths=1,
+            node_size=1000,
+            node_color='pink',
+            alpha=0.9,
+            labels={node[0]: node[1] for node in list(self.graph_chaos.nodes.data("value"))}
+        )
+       value_labels = nx.get_edge_attributes(self.graph_chaos, 'value')
+       frequency_labels = nx.get_edge_attributes(self.graph_chaos, 'distance')
+       edge_labels = {key: f'{value_labels[key]} - {frequency_labels[key]}' for key in value_labels}
+       nx.draw_networkx_edge_labels(
+            self.graph_chaos,
+            pos,
+            edge_labels=edge_labels
+        )
+       plt.axis('off')
+       plt.show()
 
-    
 def get_this_sequence_letter(x_axis_position, y_axis_position):
     if y_axis_position < 8 :
         if x_axis_position < 8:
@@ -85,21 +110,24 @@ def get_this_sequence_letter(x_axis_position, y_axis_position):
         else :
             return 'T'
 
-
 if __name__ == '__main__':
     sequence1 = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
     graph1 = ChaosGraph(sequence1, 8)
+    print("---Grafo 1 ---")
     for node, attrs in graph1.graph_chaos.nodes(data=True):
         print(f"Nodo {node}: {attrs}")
+    graph1.plot_graph()
 
     sequence2 = 'CCCCCCCCCCCCCCCCCCCCCCCCCTTTTAAAAAAAAAAAAAAAAAAAAAAAAA'
     graph2 = ChaosGraph(sequence2, 8)
     print("---Grafo 2 ---")
     for node, attrs in graph2.graph_chaos.nodes(data=True):
         print(f"Nodo {node}: {attrs}")
+    graph2.plot_graph()
 
     sequence3 = 'ACTACTTCACTTCTTCACTTCTTCGGCGGCTTACTTCTCACTTCTCACTTCTTCGGCGGCCACTTCTTCGGCGGC'
     graph3 = ChaosGraph(sequence3, 8)
     print("---Grafo 3 ---")
     for node, attrs in graph3.graph_chaos.nodes(data=True):
         print(f"Nodo {node}: {attrs}")
+    graph3.plot_graph()    
